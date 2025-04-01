e46f82fa041fd374e882205599937caf47537095
Igor <webdevbiv@gmail.com>
2025-04-01 12:11:15 +0300

    UPD | LoginForm

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,14 @@ import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
+import { LoginForm } from "./components/Form";
 
 function App() {
-  return <></>;
+  return (
+    <>
+      <LoginForm />
+    </>
+  );
 }
 
 export default App;
---
+++ src/components/Form.jsx
@@ -0,0 +1,9 @@
+export const LoginForm = () => {
+  return (
+    <form>
+      <input type="text" name="login" />
+      <input type="password" name="password" />
+      <button type="submit">Log in</button>
+    </form>
+  );
+};
