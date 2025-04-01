76475cb3f0a5e85d9160971b3d48cb6ff4be6ce2
Igor <webdevbiv@gmail.com>
2025-04-01 12:16:03 +0300

    UPD | Form

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,6 +1,17 @@
 export const LoginForm = () => {
+  const handleSubmit = (evt) => {
+    evt.preventDefault();
+    console.log(evt);
+
+    const form = evt.target;
+    const { login, password } = form.elements;
+
+    console.log(form.elements);
+    console.log(`Login: ${login.value}, Password: ${password.value}`);
+  };
+
   return (
-    <form>
+    <form onSubmit={handleSubmit}>
       <input type="text" name="login" />
       <input type="password" name="password" />
       <button type="submit">Log in</button>
