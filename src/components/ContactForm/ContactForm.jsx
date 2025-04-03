2f34b05ed59cd010e89600b85d48f2b0835889b2
Igor <webdevbiv@gmail.com>
2025-04-03 18:49:34 +0300

    ADD | validation

--- src/components/ContactForm/ContactForm.jsx
+++ src/components/ContactForm/ContactForm.jsx
@@ -1,18 +1,37 @@
 import { useId } from "react";
-import { Field, Form, Formik } from "formik";
+import { Field, Form, Formik, ErrorMessage } from "formik";
+import * as Yup from "yup";
+
+const FeedbackSchema = Yup.object().shape({
+  name: Yup.string()
+    .min(3, "Too Short!")
+    .max(50, "Too Long!")
+    .required("Required"),
+  number: Yup.string()
+    .matches(/^[0-9-]+$/, "Phone number can only include digits and hyphens")
+    .min(3, "Too Short!")
+    .max(50, "Too Long!")
+    .required("Required"),
+});
 
 const ContactForm = ({ initialValues, onSubmit }) => {
   const nameFieldId = useId();
   const numberFieldId = useId();
 
   return (
-    <Formik initialValues={initialValues} onSubmit={onSubmit}>
+    <Formik
+      initialValues={initialValues}
+      onSubmit={onSubmit}
+      validationSchema={FeedbackSchema}
+    >
       <Form>
         <label htmlFor={nameFieldId}>Name</label>
         <Field id={nameFieldId} name="name" />
+        <ErrorMessage name="name" component="span" />
 
         <label htmlFor={numberFieldId}>Number</label>
         <Field id={numberFieldId} name="number" type="tel" />
+        <ErrorMessage name="number" component="span" />
 
         <button type="submit">Add Contact</button>
       </Form>
